39fc69d776eb2b7d8cb561a77c5b8ab9495adee6
Max75020 <maxime.duplaissy@gmail.com>
2024-07-18 13:52:33 +0200

    Accès aux données API fonctionnel

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
+import { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [max, setMax] = useState("");
+	useEffect(() => {
+		const fetchMax = async () => {
+			try {
+				const response = await fetch("http://localhost:8741/api/maxes/1");
+				const data = await response.json();
+				setMax(data);
+			} catch (error) {
+				console.error("Failed to fetch services:", error);
+			}
+		};
+
+		fetchMax();
+	}, []);
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit OLL <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
